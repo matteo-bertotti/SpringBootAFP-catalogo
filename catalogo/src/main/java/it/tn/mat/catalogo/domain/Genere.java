ba2538925bd2dd3267d58d88cf65d6eef8983a52
Matteo <118365516+matteo-bertotti@users.noreply.github.com>
2026-04-12 12:49:19 +0000

    feat: :sparkles: Add Genere enum for film genres

---
+++ catalogo/src/main/java/it/tn/mat/catalogo/domain/Genere.java
@@ -0,0 +1,12 @@
+package it.tn.mat.catalogo.domain;
+
+public enum Genere {
+    AZIONE,
+    COMMEDIA,
+    DRAMMATICO,
+    HORROR,
+    FANTASCIENZA,
+    ROMANTICO,
+    THRILLER,
+    ANIMAZIONE
+}
