29cd14690dddb6b62e6b4f494c6e65258e363762
Matteo <118365516+matteo-bertotti@users.noreply.github.com>
2026-04-12 12:49:07 +0000

    feat: :sparkles: Add FilmForm class with validation annotations

---
+++ catalogo/src/main/java/it/tn/mat/catalogo/domain/FilmForm.java
@@ -0,0 +1,29 @@
+package it.tn.mat.catalogo.domain;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FilmForm {
+    @NotEmpty
+    @Size(max = 50)
+    private String titolo;
+
+    @NotEmpty
+    @Size(max = 50)
+    private String autore;
+
+    @NotEmpty
+    private Genere genere;
+
+    @Min(1888)
+    @Max(2100)
+    private int annoPubblicazione;
+}
