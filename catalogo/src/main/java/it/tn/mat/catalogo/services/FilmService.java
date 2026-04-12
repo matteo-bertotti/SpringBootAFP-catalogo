d3bb19b3d6c78ca56e8bca9f469807efb28f9385
Matteo <118365516+matteo-bertotti@users.noreply.github.com>
2026-04-12 13:12:45 +0000

    feat: :sparkles: Implement FilmService class for film management operations

---
+++ catalogo/src/main/java/it/tn/mat/catalogo/services/FilmService.java
@@ -0,0 +1,50 @@
+package it.tn.mat.catalogo.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import it.tn.mat.catalogo.domain.Film;
+import it.tn.mat.catalogo.domain.FilmForm;
+import it.tn.mat.catalogo.repositories.FilmRepository;
+
+@Service
+public class FilmService {
+    private FilmRepository filmRepository;
+    
+    // Elenco films
+    public List<Film> elencoCatalogo() {
+        return filmRepository.findAll();
+    }
+
+    // Aggiunta film
+    public Film newFilm(FilmForm filmForm) {
+        Film film = new Film();
+        film.setTitolo(filmForm.getTitolo());
+        film.setAutore(filmForm.getAutore());
+        film.setGenere(filmForm.getGenere().name());
+        film.setAnnoPubblicazione(filmForm.getAnnoPubblicazione());
+        return filmRepository.save(film);
+    }
+
+    // 
+    public Film filmPerId(UUID id) {
+        return filmRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Film non trovato per UUID: " + id));
+    }
+
+    // Eliminazione film per id
+    public void deleteSingolo(UUID id) {
+        if (!filmRepository.existsById(id)) {
+            throw new IllegalArgumentException("Film non trovato per UUID: " + id);
+        }
+        filmRepository.deleteById(id);
+    }
+
+    // Eliminazione di tutti i film
+    public void deleteAll() {
+        filmRepository.deleteAll();
+    }
+
+}
